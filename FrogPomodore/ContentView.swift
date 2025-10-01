27e1c2090c5148e4eea5d00eb81e808c3e2f425e
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-10-01 15:54:39 -0300

    Initial Commit

---
+++ FrogPomodore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FrogPomodore
+//
+//  Created by Rodrigo Cerqueira Reis on 01/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FrogPomodore/FrogPomodoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrogPomodoreApp.swift
+//  FrogPomodore
+//
+//  Created by Rodrigo Cerqueira Reis on 01/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FrogPomodoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
