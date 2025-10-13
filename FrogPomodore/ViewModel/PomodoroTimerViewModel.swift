26816450c520ab2418821f0f7f3f8d501e518858
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-10-13 16:34:08 -0300

    feat: Implement timer countdown logic in ViewModel

--- FrogPomodore/ViewModel/PomodoroTimerViewModel.swift
+++ FrogPomodore/ViewModel/PomodoroTimerViewModel.swift
@@ -19,14 +19,46 @@ final class PomodoroTimerViewModel: ObservableObject {
         isTimerRunning ? "Pause" : "Start"
     }
     
+    // MARK: - Private Properties
+    private var timerTask: Task<Void, Error>?
+    private var totalSeconds: Int = 25 * 60 // 25 minutes
+    
     // MARK: - Public Methods
     func handleToggleButtonTap() {
         isTimerRunning.toggle()
         
         if isTimerRunning {
-            print("Timer has started.")
+            startTimer()
         } else {
-            print("Timer has paused.")
+            pauseTimer()
         }
+    }
+    
+    // MARK: - Private Timer Methods
+    private func startTimer() {
+        timerTask?.cancel()
+        
+        timerTask = Task {
+            while totalSeconds > 0 {
+                try await Task.sleep(for: .seconds(1))
+                
+                try Task.checkCancellation()
+                
+                totalSeconds -= 1
+                updateTimeRemaining()
+            }
+            
+            isTimerRunning = false
+        }
+    }
+    
+    private func pauseTimer() {
+        timerTask?.cancel()
+    }
+    
+    private func updateTimeRemaining() {
+        let minutes = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        self.timeRemaining = String(format: "%02d:%02d", minutes, seconds)
     }
 }
